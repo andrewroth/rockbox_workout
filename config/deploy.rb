8da4bf989ac29c0ccfb36909b169ee7d985cb58d
Andrew Roth <andrewroth@gmail.com>
2010-12-15 21:11:37 -0500

    oops, search and replace did too much

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,14 +22,14 @@ set :git_enable_submodules, 1
 
 desc "Restart the web server"
 deploy.task :restart, :roles => :app do
-    run "touch #{deploy_to}/current/transfer/restart.txt"
+    run "touch #{deploy_to}/current/tmp/restart.txt"
 end 
 
 after 'deploy:update_code', 'local_symlinks'
 desc "Add linked files after deploy and set permissions"
 task :local_symlinks, :roles => :app do
   run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  run "ln -s #{shared_path}/transfer/sessions #{release_path}/transfer/sessions"
+  run "ln -s #{shared_path}/tmp/sessions #{release_path}/tmp/sessions"
   sudo "chgrp -R #{fetch(:group)} #{release_path}"
   sudo "chmod -R g+w #{release_path}"
 end
